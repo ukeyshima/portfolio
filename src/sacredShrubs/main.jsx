3ffce7cf580ad730d1b30908d98d255d01d69f43
Yuki Shimada <ukeyshima@gmail.com>
2019-02-05 19:23:50 +0900

    add sacredShrubs

---
+++ src/sacredShrubs/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CreateCanvas from './createCanvas';
+import style from './style.scss';
+import { Provider, inject, observer } from 'mobx-react';
+import State from './store';
+
+const stores = {
+  state: new State()
+};
+
+export default class ShadertoyStart extends React.Component {
+  componentDidMount() {
+    style.use();
+  }
+  componentWillUnmount() {
+    style.unuse();
+  }
+  render() {
+    return (
+      <Provider {...stores}>
+        <React.Fragment>
+          <CreateCanvas />
+        </React.Fragment>
+      </Provider>
+    );
+  }
+}
